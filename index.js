1714441dd87b9242a4bbeed82a7ce44d321a0209
key <54595444+fowwo@users.noreply.github.com>
2024-03-30 22:31:26 -0500

    Add Twitch client

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 import { server, playSound } from "./http.js";
+import { Client as TwitchClient } from "tmi.js";
 
 const port = 8080;
 
 server.listen(port, () => {
 	console.log(`HTTP server is listening on port ${port}.`);
+	
+	// Connect to Twitch chat.
+	new TwitchClient({ channels: [ "nitemera" ] })
+		.on("message", (channel, tags, message, self) => {
+			message.trim().split(/\s+/)
+				.filter(x => x.startsWith("!")) // Filter out non-commands.
+				.forEach(x => playSound(x.slice(1))); // Play the sound.
+		})
+		.connect().then(([ uri, port ]) => {
+			console.log(`Successfully connected to ${uri}:${port}.`);
+		});
 });
